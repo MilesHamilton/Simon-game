3cd0677351c8a3d45bf42dbd57259683f5c3587a
Miles <mham7714@gmail.com>
2020-05-13 10:26:48 -0400

    refrectored code, simplified and renamed fn's

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
 let blocks = document.querySelectorAll('.gameBlock > .block')
-let compArray = []
-let playerArray = []
+// stores blocks form iterator()
+let generatedSequence = []
+// stores blocks from playerClick()
+let playerCreatedSequence = []
 
 
 // iterates through an array of blocks pushes result into player array
-let iterator = () => {
-    let randomBlocks = blocks[Math.floor(Math.random() * blocks.length)]
-    compArray.push(randomBlocks)
-    return randomBlocks
-} 
-iterator()
-
-let playerClicks = () => {
-    blocks.forEach((element, index) => {
-        element.addEventListener('click', ()=>{
-            playerArray.push(element)
-            if(playerArray[0] === compArray[0]){
-                placeholderfunction
-            }
-        })
-
-    }) 
-    return blocks
+let getRandomBlock = (blocks) => {
+    // randomly selecting a block from blocks array
+    return blocks[Math.floor(Math.random() * blocks.length)]
 }
-playerClicks()
-console.log(compArray)
-console.log(playerArray)
+generatedSequence.push(getRandomBlock(blocks))
+
+let addClickEvent = (element) => {
+    element.addEventListener('click', ()=>{
+        callAdding()
+    })
+}
+console.log(addEventListener)
+
+let callClickEvent = (blocks) => {
+    blocks.forEach(block => {
+        addEventListener(block)
+    });
+}
+
+// player clicks block, block is pushed to new array, compared, game ends/game continues
+// let playerClicks = () => {
+//     blocks.forEach((element, index) => {
+//         element.addEventListener('click', () => {
+//             playerArray.push(element)
+//             if (playerArray[index] === compArray[index]) {
+//                 iterator()
+//             }
+//         })
+
+//     })
+//     return blocks
+// }
+// playerClicks()
+
+// checks every item in an array 
+let incrementor = (array, delegate, delay) => {
+    let i = 0
+
+    function loop() {
+        // each loop, call passed in function
+        delegate(array[i]);
+
+        // increment, and if we're still here, call again
+        if (i++ < array.length - 1)
+            setTimeout(loop, delay); //recursive
+    }
+
+    // seed first call
+    setTimeout(loop, delay);
+}
+//   incrementor(compArray, iterator(), 2000)
 
 
+console.log(generatedSequence)
+console.log(playerCreatedSequence)
